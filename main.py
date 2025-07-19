93d2b738985e521ca3e1406b3eca13b668a4e45f
hhemantpatel <patelhemant.7509@gmail.com>
2025-07-20 00:07:17 +0530

    update main.py and add sql file

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 # Step 1: Connect to MySQL
 conn = mysql.connector.connect(
-    host="localhost",        # or "127.0.0.1"
-    user="root",             # your MySQL username
-    password="Patel.7509",# your MySQL password
-    database="class" # the database you created
+    host="localhost",        
+    user="root",             
+    password="Patel.7509",
+    database="experiments" 
 )
 
 # Step 2: Query data into a Pandas DataFrame
-query = "SELECT * FROM dat"
+query = "SELECT * FROM data"
 df = pd.read_sql(query, conn)
 
 print(df.head())
